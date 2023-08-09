931fd40b736b088473a8ada06e8bda2139198a13
Willy Gutsche <willy.gutsche@unifesp.br>
2023-08-09 19:27:19 -0300

    Add loading component

--- src/components/Products/Products.jsx
+++ src/components/Products/Products.jsx
@@ -3,20 +3,31 @@ import fetchProducts from '../../api/fetchProducts';
 
 import './Products.css';
 import ProductCard from '../ProductCard/ProductCard';
+import Loading from '../Loading/Loading';
 
 function Products() {
     const [products, setProducts] = useState([]);
+    const [isLoading, setIsLoading] = useState(true);
 
     useEffect( () => {
-        fetchProducts('promoção').then( response => 
-            setProducts(response)
-        );
+        fetchProducts('promoção').then( response => {
+            setProducts(response);
+            setIsLoading(false);
+        });
     }, []);
 
+    function getProductCards() {
+        return products.map( product => 
+            <ProductCard key={ product.id } product={ product } /> 
+        );
+    }
+
     return (
-        <section className="products container">
-            { products.map( product => <ProductCard key={ product.id } product={ product } />) }
-        </section>
+        (isLoading ? <Loading /> : 
+            <section className="products container">
+                { getProductCards() }
+            </section>
+        )
     );
 }
 
