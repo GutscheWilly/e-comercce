18d0e5578af05aebaba9b9d799794bf96439975a
Willy Gutsche <willy.gutsche@unifesp.br>
2023-08-18 20:55:52 -0300

    Change search bar and add app icon

---
+++ src/components/ShopIcon/ShopIcon.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import { BiSolidStore } from 'react-icons/bi';
+
+import './ShopIcon.css';
+
+function ShopIcon() {
+    return <div className="shop__icon"><BiSolidStore /></div>;
+}
+
+export default ShopIcon;
